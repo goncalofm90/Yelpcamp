b61c0c0778238216e821ede69dc94b8f4df302ed
goncalofm90 <65180829+goncalofm90@users.noreply.github.com>
2020-06-04 14:08:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(flash());
 app.locals.moment = require('moment');
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
-    secret: "Once again Rusty wins cutest dog!",
+    secret: "Hello World!",
     resave: false,
     saveUninitialized: false
 }));
